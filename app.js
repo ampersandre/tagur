bc224af7964cfe38f28f3fe281b3d2b4c8491036
ampersandre <mougeot.andre@gmail.com>
2014-04-21 17:31:58 -0600

    Revert "Major changes"
    This reverts commit 49e528a71f635b9ca952710bb47e63dc89401516.

--- app.js
+++ app.js
@@ -1,70 +1,81 @@
 var express = require('express');
-var path = require('path');
-var favicon = require('static-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
-var fs = require('fs');
-
 var app = express();
+app.use(bodyParser());
+var mu = require ('mu2');
+mu.root = __dirname + '/templates';
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://ampersandre:password@ds049337.mongolab.com:49337/reddit');
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hjs');
+var dbImage = mongoose.model('Image', new mongoose.Schema({
+	url: String,
+	comments: [new mongoose.Schema({
+		x: Number,
+		y: Number,
+		text: String,
+		date: { type: Date, 'default': Date.now }
+	})]
+}));
 
-app.use(favicon());
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
-app.use(cookieParser());
-app.use(require('less-middleware')({ src: path.join(__dirname, 'public') }));
-app.use(express.static(path.join(__dirname, 'public')));
 
-/// catch 404 and forwarding to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-
-/// error handlers
-
-// development settings
-if (app.get('env') === 'development') {
-    mongoose.connect('mongodb://ampersandre:password@ds049337.mongolab.com:49337/reddit');
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
+function saveImage(req, res, objectId) {
+	if (!(req.body.hasOwnProperty('src') || objectId) ||
+		!req.body.hasOwnProperty('x') || !req.body.hasOwnProperty('y') ||
+		!req.body.hasOwnProperty('comment')) {
+		res.statusCode = 400;
+		return res.send('Error 400: Post syntax incorrect');
+	}
+	var query = {};
+	var imageDetails = {};
+	var newComment = { x: parseInt(req.body.x), y: parseInt(req.body.y), text: req.body.comment };
+	if (objectId) { query._id = objectId; }
+	else {
+		query.url = req.body.src;
+		imageDetails.url = req.body.src;
+	}
+	
+	dbImage.findOneAndUpdate(query,
+		{
+			$set: imageDetails,
+			$push: { comments: newComment }
+		}, { upsert: true },
+		function(err, image) {
+			if (err) return console.error(err);
+			res.send(image);
+		});
 }
-
-// Production settings
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+app.post('/image', function(req, res) {
+	saveImage(req, res);
+});
+app.post('/image/:id', function(req, res) {
+	var objectId = req.params.id;
+	saveImage(req, res, objectId);
+});
+app.get('/images', function(req, res) {
+	dbImage.find(function(err,images) {
+		if (err) return console.error(err);
+		res.json(images);
+	});
+});
+app.get('/image/:id', function(req, res) {
+	var objectId = req.params.id;
+	dbImage.find({_id: objectId}, function(err, image) {
+		if (err) return console.error(err);
+		res.json(image);
+	});
 });
 
-// Require all the models
-fs.readdirSync(__dirname + '/models').forEach(function(filename){
-    if (~filename.indexOf('.js')) { require(__dirname + '/models/'+ filename); }
-})
-
-// Load routes
-var routes = require('./routes/index');
-app.use('/', routes);
-
-module.exports = app;
-
-app.set('port', process.env.PORT || 3000);
-var debug = require('debug')('tagur');
-
-var server = app.listen(app.get('port'), function() {
-    debug('Express server listening on port ' + server.address().port);
+app.get('/new', function(req, res) {
+	mu.compileAndRender('new.html').pipe(res);
 });
+app.post('/new/:url', function(req, res) {
+	var url = req.params.url;
+	dbImage.find({url: url}, function(err, image) {
+		if (err) return console.error(err);
+		res.json(image);
+	});
+});
+
+app.use(express.static(__dirname+'/public'));
+
+app.listen(process.env.PORT || 8080);
