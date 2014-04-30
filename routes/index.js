a9f77af713c7b5a866184f1c095d37f28b655e2a
Andre Mougeot <mougeot.andre@gmail.com>
2014-04-29 19:45:13 -0600

    UI updates

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,14 @@ var request = require('request');
 var Image = mongoose.model('Image');
 var fs = require('fs');
 
+function mustache(res, template, params) {
+    if (!params.partials) { params.partials = {}; }
+    params.partials.forms = 'forms';
+    params.partials.header = 'header';
+    params.partials.footer = 'footer';
+    res.render(template, params)
+}
+
 function saveImage(req, res, objectId) {
     if (!(req.body.hasOwnProperty('src') || objectId) ||
         !req.body.hasOwnProperty('xP') || !req.body.hasOwnProperty('yP') ||
@@ -38,11 +46,11 @@ router.post('/image/upload', function(req, res) {
     request({
         url: 'https://api.imgur.com/3/image.json',
         method: 'POST',
-        //form: { image: 'http://'+req.headers.host+'/uploads/'+req.files.uploadedImage.name },
-        form: { image: 'http://i.imgur.com/qrkXOA2.jpg' }, // for testing on localhost
+        form: { image: 'http://'+req.headers.host+'/uploads/'+req.files.uploadedImage.name },
+        //form: { image: 'http://i.imgur.com/qrkXOA2.jpg' }, // for testing on localhost
         headers: { 'Authorization': 'Client-ID 2964e421ec33193' }
     },function(error, response, body){
-        res.render('image', { partials: { forms: 'forms' },src: JSON.parse(body).data.link});
+        mustache(res, 'image', {src: JSON.parse(body).data.link});
         fs.unlink(req.files.uploadedImage.path);
     });
 });
@@ -53,7 +61,7 @@ router.post('/image/:id', function(req, res) {
 router.get('/image/:id', function(req, res) {
     var objectId = req.params.id;
     Image.findById(objectId, function(err, image) {
-        res.render('image', { partials: { forms: 'forms' }, image: image });
+        mustache(res, 'image', { image: image });
     });
 });
 router.get('/images', function(req, res) {
@@ -62,7 +70,6 @@ router.get('/images', function(req, res) {
     });
 });
 router.post('/new', function(req,res) {
-    console.log(req.body);
     if (!req.body.hasOwnProperty('src')) {
         res.statusCode = 400;
         return res.send('Error 400: Post syntax incorrect');
@@ -70,14 +77,13 @@ router.post('/new', function(req,res) {
     var src = req.body.src;
     Image.find({src: src}, function(err, image) {
         if (err) return console.error(err);
-        res.render('image', { partials: { forms: 'forms' }, image: image, src: src });
+        mustache(res, 'image', { image: image, src: src });
     });
 });
 
 router.get('/', function(req, res) {
-    Image.find().limit(9).exec(function(err, images) {
-        console.log(images);
-        res.render('index', { partials: { forms: 'forms' }, images: images });
+    Image.find().limit(6).exec(function(err, images) {
+        mustache(res, 'index', { images: images });
     });
 });
 
